4855df98c16e813f5c5c4bbb2e055aaf090121a1
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:23:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-names = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 # Step 1,2,3,4,5 as one, abstractly
 def badge_maker
