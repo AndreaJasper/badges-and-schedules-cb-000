674e13778927c4d0dedff3f024f9cc7180937bf9
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:09:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  badges = []
+
+end
+
+def assign_rooms(attendees)
+  while attendees < 7
+    attendees += 1
+  end
+
+end
+
+def printer
+  puts "Hello, #{name}! You'll be assigned to room #{room}!"
+end
