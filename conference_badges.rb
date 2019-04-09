52dacf07c72eaf50cb57ced86d3d36356dc22341
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:34:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,16 @@
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 room_num = [1,2,3,4,5,6,7]
 
-def badge_maker(names)
-  names.each do |name|
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each do |name|
+    array.push ("Hello, my name is #{name}.")
   end
+  return array;
 end
 
 
@@ -16,6 +23,6 @@ end
 
 
 def batch_badge_creator
-  puts "Hello, my name is #{name}."
+  
   puts "Hello, my name is #{badge_maker}! You'll be assigned to room #{counter}!"
 end
