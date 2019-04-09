81711255269a3a1cf9828c3e5508fbecfc43eb87
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:26:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,11 +7,16 @@ def badge_maker
   end
 end
 
-
-def conference_badges
+room_num = [1,2,3,4,5,6,7]
+def assign_rooms
   counter = 1
-  names.each do |name|
-    puts "Hello, my name is #{name}. I'm in room #{counter}"
+  room_num.each do |room|
+    puts "Hello, my name is #{badge_maker}! You'll be assigned to room #{counter}!"
     counter += 1
   end
+end
+
+
+def batch_badge_creator
+
 end
