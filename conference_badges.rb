96f54fad8800093c35ff81130fa247b87b71c61c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:13:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 names = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
- 
+
 # Step 1,2,3,4,5 as one, abstractly
 def conference_badges
   names.each do |name|
