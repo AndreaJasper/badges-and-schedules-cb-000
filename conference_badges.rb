d4366745a305cd08f6b738fa930ee607c2e73bd6
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:23:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,17 @@
 names = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
 
 # Step 1,2,3,4,5 as one, abstractly
-def conference_badges
+def badge_maker
   names.each do |name|
     puts "Hello, my name is #{name}."
+  end
+end
+
+
+def conference_badges
+  counter = 1
+  names.each do |name|
+    puts "Hello, my name is #{name}. I'm in room #{counter}"
+    counter += 1
   end
 end
