47995c6a9bc7dd4582f1e98b30cb90a5f4724ba3
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:29:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,19 @@ room_num = [1,2,3,4,5,6,7]
 
 def badge_maker(names)
   names.each do |name|
-    puts "Hello, my name is #{name}."
   end
 end
 
 
-def assign_rooms(names, room_num)
+def assign_rooms(room_num)
   counter = 1
   room_num.each_with_index |room|
-    puts "Hello, my name is #{badge_maker}! You'll be assigned to room #{counter}!"
     counter += 1
   end
 end
 
 
 def batch_badge_creator
-
+  puts "Hello, my name is #{name}."
+  puts "Hello, my name is #{badge_maker}! You'll be assigned to room #{counter}!"
 end
