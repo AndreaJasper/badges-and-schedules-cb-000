55bec4b7c178ffa49ceb68c7b6dbaf17f4b8419f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-09 15:20:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,7 @@ names = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","
 
 # Step 1,2,3,4,5 as one, abstractly
 def conference_badges
-  count = 0
-  while count <= names.length-1
-    puts "Hello, my name is #{name[count]}."
-    name += 1
+  names.each do |name|
+    puts "Hello, my name is #{name}."
   end
 end
